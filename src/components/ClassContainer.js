77c834f9ed4b5b1ce90e2cac04e9c395db9ae4b9
ironman-eric <eonno123@yahoo.com>
2023-09-19 20:23:47 -0700

    fix weights

--- src/components/ClassContainer.js
+++ src/components/ClassContainer.js
@@ -4,12 +4,12 @@ import dndClass from '../data/dndClass';
 function ClassContainer({ onChangeValue }) {
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <div className="px-6 py-4">
+      <div className="px-2 py-2">
         <div className="font-bold text-purple-500 text-xl mb-2">
           Class
         </div>
       </div>
-      <div className="px-6 py-4">
+      <div className="px-2 py-2">
         <AttributePicker name="dndClass" items={Object.keys(dndClass)} onOptionChange={onChangeValue} />
       </div>
     </div>
